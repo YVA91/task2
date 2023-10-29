7319ff58c18c9908dbc184d4585a72d10ae49b10
Vladimir Iakhnev <Yahnev91@yandex.ru>
2023-10-29 15:11:33 +0300

    Homework 3

---
+++ lesson3/task1/task1.cpp
@@ -0,0 +1,66 @@
+﻿
+#include <iostream>
+#include <locale>
+#include <Windows.h>
+#include <exception>
+#include <string>
+
+
+class Smart_array {
+private:
+	int size = 0;
+	int* arr;
+	int count = 0;
+public:
+	Smart_array (int a_size) {
+		size = a_size;
+		arr = new int[size];
+	}
+	~ Smart_array() {
+		delete[] arr;
+	}
+
+	void add_element(int a) {
+		if (count < size) {
+			arr[count] = a;
+			count++;
+		}
+		else {
+			throw std::exception("Слишком много элементов");
+		}
+	}
+
+	int get_element(int b) {
+		if (b >= 0 && b < size) {
+			return arr[b];
+		}
+		else {
+			throw std::length_error("Такого элемента не существует");
+		}
+	}
+
+
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	try {
+		Smart_array arr(5);
+		arr.add_element(1);
+		arr.add_element(4);
+		arr.add_element(155);
+		arr.add_element(14);
+		arr.add_element(15);
+		std::cout << arr.get_element(1) << std::endl;
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	} 
+	catch (const std::length_error& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+}
---
+++ lesson3/task2/task2.cpp
@@ -0,0 +1,76 @@
+﻿
+#include <iostream>
+#include <locale>
+#include <Windows.h>
+#include <exception>
+#include <string>
+#include <algorithm>
+
+
+
+class smart_array {
+public:
+	int size = 0;
+	int* arr;
+	int count = 0;
+
+public:
+	smart_array(int a_size) {
+		size = a_size;
+		arr = new int[size];
+	}
+	~smart_array() {
+		delete[] arr;
+	}
+
+	void add_element(int a) {
+		if (count < size) {
+			arr[count] = a;
+			count++;
+		}
+		else {
+			throw std::exception("Слишком много элементов");
+		}
+	}
+
+	int get_element(int b) {
+		if (b >= 0 && b < size) {
+			return arr[b];
+		}
+		else {
+			throw std::length_error("Такого элемента не существует");
+		}
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	try {
+		smart_array arr(5);
+		arr.add_element(1);
+		arr.add_element(4);
+		arr.add_element(155);
+
+		smart_array new_array(2);
+		new_array.add_element(44);
+		new_array.add_element(34);
+
+		std::cout << arr.get_element(1) << std::endl;
+		std::cout << new_array.get_element(1) << std::endl;
+		
+		for (int i = 0; i < arr.size; i++) {
+			arr.arr[i] = new_array.arr[i];
+		}
+
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+	catch (const std::length_error& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+}
