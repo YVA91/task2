cb0c81a0bef10ced7ce6413fbf81f320a39c784e
Vladimir Iakhnev <Yahnev91@yandex.ru>
2024-01-07 18:44:22 +0300

    Homework 7

---
+++ lesson7/task1/task1.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+#include <locale>
+#include <Windows.h>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::vector <int> v{ 1, 1, 2, 5, 6, 1, 2, 4 };
+
+    std::cout << "[IN] ";
+    for (const auto& elem : v) {
+        std::cout << elem << " ";
+    }
+    std::cout << "\n";
+
+    std::sort(v.begin(), v.end());
+
+    auto last = std::unique(v.begin(), v.end());
+
+    for (const auto& elem : v) {
+        std::cout << elem << " ";
+    }
+    std::cout << "\n";
+
+    v.erase(last, v.end());
+
+    std::cout << "[OUT] ";
+    for (const auto& elem : v) {
+        std::cout << elem << " ";
+    }
+    std::cout << "\n";
+    
+}
+
---
+++ lesson7/task2/task2.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+#include <locale>
+#include <Windows.h>
+#include <vector>
+#include <algorithm>
+
+#include <list>
+#include <set>
+
+template <typename T>
+void print_container(const T& container) {
+	auto elem = container.begin();
+	while (elem != container.end()) {
+		std::cout << *elem << " ";
+		elem++;
+	}
+	std::cout << "\n";
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	print_container(test_set); 
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	print_container(test_list); 
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	print_container(test_vector); 
+}
+
